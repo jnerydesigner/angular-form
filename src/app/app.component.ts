9fd75995b8d304bcaa4e0cf2dc8a706fe318e3a8
jnerydesigner <jander.webmaster@gmail.com>
2024-05-18 15:12:31 -0400

    Feat: Create Form and submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./style.component.css'],
 })
--- src/app/form-cont/form-cont.component.ts
+++ src/app/form-cont/form-cont.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { Contact } from '../models/contact.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-cont',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-cont.component.html',
   styleUrl: './form-cont.component.css',
 })
-export class FormContComponent {}
+export class FormContComponent implements OnInit {
+  formContact: FormGroup;
+  submittedData: any;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formContact = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.createForm(new Contact());
+  }
+
+  createForm(contact: Contact) {
+    this.formContact = this.formBuilder.group({
+      name: [contact.name, Validators.required],
+      email: [contact.email, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submittedData = this.formContact.value;
+
+    console.log(this.submittedData);
+
+    // this.formContact.reset();
+  }
+}
